637587ab4d4651be5502237e823b160d975ac4f4
AngieValverdeSalazar <89101151+AngieValverdeSalazar@users.noreply.github.com>
2022-09-28 00:07:13 -0500

    feat(devices): added devices-api.server.js

---
+++ src/repair-it/services/devices-api.service.js
@@ -0,0 +1,21 @@
+import http from '../../core/services/http-common';
+export class DevicesApiService {
+getAll() {
+return http.get('/devices');
+}
+getById(id) {
+return http.get(`/devices/${id}`);
+}
+create(data) {
+return http.post('/devices', data);
+}
+update(id, data) {
+return http.put(`/devices/${id}`, data);
+}
+delete(id) {
+return http.delete(`/devices/${id}`);
+}
+findByModelId(modelId) {
+return http.get(`/devices?modelId=${modelId}`);
+}
+}
