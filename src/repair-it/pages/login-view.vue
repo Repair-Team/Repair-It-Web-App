796e006fa08fbc5b93c3cfefc4457775cda53e03
JCayllahuaG <u202019558@upc.edu.pe>
2022-09-28 20:29:10 -0500

    feat: added login view

---
+++ src/repair-it/pages/login-view.vue
@@ -0,0 +1,18 @@
+<template>
+
+  <sign-in-component></sign-in-component>
+
+</template>
+<script>
+import SignInComponent from "../components/sign-in.component.vue";
+export default {
+  name: "login-view",
+  components: {SignInComponent},
+  data(){
+
+  },
+  created() {
+
+  }
+}
+</script>
