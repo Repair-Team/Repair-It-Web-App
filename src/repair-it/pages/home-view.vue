d7343c244d2f2f9fa4f9f78a988d2c35701120ea
JCayllahuaG <u202019558@upc.edu.pe>
2022-09-28 20:27:57 -0500

    feat: home-view added

---
+++ src/repair-it/pages/home-view.vue
@@ -0,0 +1,51 @@
+<template>
+
+  <div class="container">
+    <img src="../../assets/Background.jpg" alt="banner" />
+      <div class="slogan">
+       The best Techniques here!
+      </div>
+  </div>
+  <div style="margin: 3rem;">
+
+    <device-card-list></device-card-list>
+
+  </div>
+</template>
+<script>
+import DeviceCardList from "../components/device/device-card-list.component.vue";
+export default {
+  name: "home-view",
+  components: { DeviceCardList},
+  data() {},
+  created() {},
+};
+</script>
+<style scoped>
+.slogan {
+  color:white;
+  position: absolute;
+  top: 28%;
+  left: 30%;
+  transform: translate(-40%, -20%);
+  font-family: "Bebas Neue", cursive;
+  font-size: 4vw;
+}
+
+.slogan span:nth-child(1) {
+  color: #e5383b;
+}
+.container {
+
+  display: inline-block;
+  text-align: center;
+  height: auto;
+
+}
+img {
+  object-fit: none;
+  width: 99vw;
+  height: 500px;
+}
+
+</style>
