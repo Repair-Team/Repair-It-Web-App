e81a0ef94826cdf373bf80711f331dc07a7916d4
JCayllahuaG <u202019558@upc.edu.pe>
2022-09-28 20:29:47 -0500

    fix: devices-list

--- src/repair-it/components/device/device-list.component.vue
+++ src/repair-it/components/device/device-list.component.vue
@@ -1,33 +1,32 @@
 <template>
   <div>
-    <div class="card">
-      <pv-toolbar class="mb-4">
-        <template #start>
-          <pv-button
+  <div class="card">
+    <pv-toolbar class="mb-4">
+      <template #start>
+        <pv-button
             label="New"
             icon="pi pi-plus"
             class="p-button-success mr-2"
             @click="openNew"
-          />
-          <pv-button
+        />
+        <pv-button
             label="Delete"
             icon="pi pi-trash"
             class="p-button-danger"
             @click="confirmDeleteSelected"
             :disabled="!selectedDevices || !selectedDevices.length"
-          />
-        </template>
-        <template #end>
-          <pv-button
+        />
+      </template>
+      <template #end>
+        <pv-button
             label="Export"
             icon="pi pi-download"
             class="p-button-help"
             @click="exportToCSV($event)"
-          />
-        </template>
-      </pv-toolbar>
-
-      <pv-data-table
+        />
+      </template>
+    </pv-toolbar>
+    <pv-data-table
         ref="dt"
         :value="devices"
         v-model:selection="selectedDevices"
@@ -36,240 +35,197 @@
         :rows="10"
         :filters="filters"
         paginatorTemplate="FirstPageLink PrevPageLink PageLinks
-    NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+      NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
         :rowsPerPageOptions="[5, 10, 25]"
         currentPageReportTemplate="Showing {first} to {last} of
-    {totalRecords} devices"
+      {totalRecords} devices"
         responsiveLayout="scroll"
-      >
-        <template #header>
-          <div
+    >
+      <template #header>
+        <div
             class="table-header flex flex-column md:flex-row md:justify-content-between"
-          >
-            <h5 class="mb-2 md:m-0 p-as-md-center text-xl">Manage Devices</h5>
-            <span class="p-input-icon-left">
-              <i class="pi pi-search" />
-              <pv-input-text
-                v-model="filters['global'].value"
-                placeholder="Search..."
-              />
-            </span>
-          </div>
-        </template>
-
-        <pv-column
+        >
+          <h5 class="mb-2 md:m-0 p-as-md-center text-xl">Manage Devices</h5>
+          <span class="p-input-icon-left">
+                <i class="pi pi-search" />
+                <pv-input-text
+                    v-model="filters['global'].value"
+                    placeholder="Search..."
+                />
+              </span>
+        </div>
+      </template>
+      <pv-column
           selectionMode="multiple"
           style="width: 3rem"
           :exportable="false"
-        ></pv-column>
-        <!--
-        <pv-column
+      ></pv-column>
+      <pv-column
           field="id"
           header="ID"
           :sortable="true"
-          style="min-width: 4rem"
-        ></pv-column>
-        -->
-        <pv-column
-          field="deviceBrandName"
-          header="Brand Name"
-          :sortable="true"
-          style="min-width: 16rem"
-        ></pv-column>
-        <pv-column
-          field="deviceModelName"
-          header="Model Name"
-          :sortable="true"
-          style="min-width: 16rem"
-        ></pv-column>
-        <!--       <pv-column
-          field="status"
-          header="Status"
-          :sortable="true"
           style="min-width: 12rem"
-        >
-          <template #body="slotProps">
-            <pv-tag
-              v-if="slotProps.data.status === 'onRepair'"
-              severity="success"
-              >{{ slotProps.data.status }}</pv-tag
-            >
-            <pv-tag v-else severity="info">{{ slotProps.data.status }}</pv-tag>
-          </template>
-        </pv-column> 
-      -->
-        <pv-column :exportable="false" style="min-width: 8rem">
-          <template #body="slotProps">
-            <pv-button
+      ></pv-column>
+      <pv-column
+          field="deviceModelId"
+          header="Device Model ID"
+          :sortable="true"
+          style="min-width: 16rem"
+      ></pv-column>
+      <pv-column
+          field="clientId"
+          header="Client ID"
+          :sortable="true"
+          style="min-width: 16rem"
+      ></pv-column>
+      <pv-column
+          field="purchaseDate"
+          header="Purchase Date"
+          :sortable="true"
+          style="min-width: 16rem"
+      ></pv-column>
+
+      <pv-column :exportable="false" style="min-width: 8rem">
+        <template #body="slotProps">
+          <pv-button
               icon="pi pi-pencil"
               class="p-button-text p-button-rounded"
               @click="editDevice(slotProps.data)"
-            />
-            <pv-button
+          />
+          <pv-button
               icon="pi pi-trash"
               class="p-button-text p-button-rounded"
               @click="confirmDeleteDevice(slotProps.data)"
-            />
-          </template>
-        </pv-column>
-      </pv-data-table>
-    </div>
-    <pv-dialog
+          />
+        </template>
+      </pv-column>
+    </pv-data-table>
+  </div>
+
+
+  <pv-dialog
       v-model:visible="deviceDialog"
       :style="{ width: '450px' }"
       header="Device Information"
       :modal="true"
       class="p-fluid"
-    >
-      <div class="field mt-3">
-        <span class="p-float-label">
-          <pv-input-text
-            type="text"
-            id="deviceBrandName"
-            v-model.trim="device.deviceBrandName"
-            required="true"
-            autofocus
-            :class="{ 'p-invalid': submitted && !device.deviceBrandName }"
-          />
-          <label for="deviceBrandName">Brand Name</label>
-          <small class="p-error" v-if="submitted && !device.deviceBrandName"
-            >Brand Name is required.</small
-          >
-        </span>
-      </div>
-      <div class="field mt-3">
-        <span class="p-float-label">
-          <pv-input-text
-            type="text"
-            id="deviceModelName"
-            v-model.trim="device.deviceModelName"
-            required="true"
-            autofocus
-            :class="{ 'p-invalid': submitted && !device.deviceModelName }"
-          />
-          <label for="deviceModelName">Model Name</label>
-          <small class="p-error" v-if="submitted && !device.deviceModelName"
-            >Model Name is required.</small
-          >
-        </span>
-      </div>
-      <!--
-      <div class="field">
-        <span class="p-float-label">
-          <pv-textarea
-            id="description"
-            v-model="device.description"
-            required="false"
-            rows="2"
-            cols="20"
-          />
-          <label for="description">Description</label>
-        </span>
-      </div>
-      -->
-      <!--
-      <div class="field">
-        <pv-dropdown
-          id="published"
-          v-model="device.status"
-          :options="statuses"
-          optionLabel="label"
-          placeholder="Select a Status"
-        >
-          <template #value="slotProps">
-            <div v-if="slotProps.value && slotProps.value.value">
-              <span :class="'device-badge status-' + slotProps.value.value">{{
-                slotProps.value.label
-              }}</span>
-            </div>
-            <div v-else-if="slotProps.value && !slotProps.value.value">
-              <span
-                :class="'device-badge status-' + slotProps.value.toLowerCase()"
-                >{{ slotProps.value }}</span
-              >
-            </div>
-            <span v-else>
-              {{ slotProps.placeholder }}
-            </span>
-          </template>
-        </pv-dropdown>
-      </div>
--->
-      <template #footer>
-        <pv-button
+  >
+    <div class="field mt-3">
+          <span class="p-float-label">
+            <pv-input-text
+                type="text"
+                id="deviceModelId"
+                v-model.trim="device.deviceModelId"
+                required="true"
+                autofocus
+                :class="{ 'p-invalid': submitted && !device.title }"
+            />
+            <label for="deviceModelId">Device id</label>
+            <small class="p-error" v-if="submitted && !device.title"
+            >Device Model Id is required.</small
+            >
+          </span>
+    </div>
+    <div class="field">
+          <span class="p-float-label">
+            <pv-input-text
+                type="text"
+                id="deviceModelId"
+                v-model.trim="device.clientId"
+                required="true"
+            />
+            <label for="deviceModelId">Client Id</label>
+          </span>
+    </div>
+    <div class="field">
+          <span class="p-float-label">
+            <pv-input-text
+                type="text"
+                id="purchaseDate"
+                v-model.trim="device.purchaseDate"
+                required="true"
+            />
+            <label for="purchaseDate">Purchase Date </label>
+          </span>
+    </div>
+
+    <template #footer>
+      <pv-button
           :label="'Cancel'.toUpperCase()"
           icon="pi pi-times"
           class="p-button-text"
-          @click="hideDialog"
-        />
-        <pv-button
+          @click="hideDialog()"
+      />
+      <pv-button
           :label="'Save'.toUpperCase()"
           icon="pi pi-check"
           class="p-button-text"
-          @click="saveDevice"
-        />
-      </template>
-    </pv-dialog>
-    <pv-dialog
+          @click="saveDevice()"
+      />
+    </template>
+  </pv-dialog>
+    <!--Dialog for delete in table-->
+  <pv-dialog
       v-model:visible="deleteDeviceDialog"
       :style="{ width: '450px' }"
       header="Confirm"
       :modal="true"
-    >
-      <div class="confirmation-content">
-        <i class="pi pi-exclamation-triangle mr-3" style="font-size: 2rem" />
-        <span v-if="device"
-          >Are you sure you want to delete <b>{{ device.deviceBrandName }}</b
-          >?</span
-        >
-      </div>
-      <template #footer>
-        <pv-button
+  >
+    <div class="confirmation-content">
+      <i class="pi pi-exclamation-triangle mr-3" style="font-size: 2rem" />
+      <span v-if="device"
+      >Are you sure you want to delete <b>{{ device.title }}</b
+      >?</span
+      >
+    </div>
+    <template #footer>
+      <pv-button
           :label="'No'.toUpperCase()"
           icon="pi pi-times"
           class="p-button-text"
           @click="deleteDeviceDialog = false"
-        />
-        <pv-button
+      />
+      <pv-button
           :label="'Yes'.toUpperCase()"
           icon="pi pi-check"
           class="p-button-text"
           @click="deleteDevice"
-        />
-      </template>
-    </pv-dialog>
-    <pv-dialog
+      />
+    </template>
+  </pv-dialog>
+
+    <!--Dialog for delete-->
+  <pv-dialog
       v-model:visible="deleteDevicesDialog"
       :style="{ width: '450px' }"
       header="Confirm"
       :modal="true"
-    >
-      <div class="confirmation-content">
-        <i class="pi pi-exclamation-triangle mr-3" style="font-size: 2rem" />
-        <span v-if="device"
-          >Are you sure you want to delete the selected devices?</span
-        >
-      </div>
-      <template #footer>
-        <pv-button
+  >
+    <div class="confirmation-content">
+      <i class="pi pi-exclamation-triangle mr-3" style="font-size: 2rem" />
+      <span v-if="device"
+      >Are you sure you want to delete the selected devices?</span
+      >
+    </div>
+    <template #footer>
+      <pv-button
           :label="'No'.toUpperCase()"
           icon="pi pi-times"
           class="p-button-text"
           @click="deleteDevicesDialog = false"
-        />
-        <pv-button
+      />
+      <pv-button
           :label="'Yes'.toUpperCase()"
           icon="pi pi-check"
           class="p-button-text"
           @click="deleteSelectedDevices"
-        />
-      </template>
-    </pv-dialog>
+      />
+    </template>
+  </pv-dialog>
   </div>
 </template>
-
 <script>
-import { DevicesApiService } from "../services/devices-api.service";
+import { DevicesApiService } from "../../services/device-api.service";
 import { FilterMatchMode } from "primevue/api";
 export default {
   name: "device-list",
@@ -283,10 +239,6 @@ export default {
       selectedDevices: null,
       filters: {},
       submitted: false,
-      statuses: [
-        { label: "onRepair", value: "onRepair" },
-        { label: "workingProperly", value: "workingProperly" },
-      ],
       devicesService: null,
     };
   },
@@ -300,20 +252,14 @@ export default {
     });
     this.initFilters();
   },
-
   methods: {
-    getDisplayableDevice(device) {
-      device.status = device.published
-        ? this.statuses[0].label
-        : this.statuses[1].label;
-      return device;
-    },
+
     getStorableDevice(displayableDevice) {
       return {
         id: displayableDevice.id,
-        deviceBrandName: displayableDevice.deviceBrandName,
-        deviceModelName: displayableDevice.deviceModelName,
-        //published: displayableDevice.status.label === "onRepair",
+        deviceModelId: displayableDevice.deviceModelId,
+        clientId: displayableDevice.clientId,
+        purchaseDate: displayableDevice.purchaseDate,
       };
     },
     openNew() {
@@ -331,30 +277,27 @@ export default {
     },
     saveDevice() {
       this.submitted = true;
-      if (this.device.deviceBrandName.trim()) {
+      if (this.device.deviceModelId) {
         if (this.device.id) {
           console.log(this.device);
           this.device = this.getStorableDevice(this.device);
           this.devicesService
-            .update(this.device.id, this.device)
-            .then((response) => {
-              console.log(response.data.id);
-              this.devices[this.findIndexById(response.data.id)] =
-                this.getDisplayableDevice(response.data);
-              this.$toast.add({
-                severity: "success",
-                summary: "Successful",
-                detail: "Device Updated",
-                life: 3000,
+              .update(this.device.id, this.device)
+              .then((response) => {
+                console.log(response.data.id);
+                this.$toast.add({
+                  severity: "success",
+                  summary: "Successful",
+                  detail: "Device Updated",
+                  life: 3000,
+                });
+                console.log(response);
               });
-              console.log(response);
-            });
         } else {
           this.device.id = 0;
           console.log(this.device);
           this.device = this.getStorableDevice(this.device);
           this.devicesService.create(this.device).then((response) => {
-            this.device = this.getDisplayableDevice(response.data);
             this.devices.push(this.device);
             this.$toast.add({
               severity: "success",
