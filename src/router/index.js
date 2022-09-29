8a5e9e2af54407f41a2303c63fe3ef372f888448
JCayllahuaG <u202019558@upc.edu.pe>
2022-09-28 20:26:57 -0500

    fix: tool-bar routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,39 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from "../../src/repair-it/pages/home-view.vue"
+import Devices from "../../src/repair-it/components/device/device-list.component.vue"
+import SigIn from "../../src/repair-it/pages/login-view.vue"
+
 const router = createRouter({
-history: createWebHistory(import.meta.env.BASE_URL),
-routes: [
-{
-path: "/home",
-name: "home",
-component: HomeView,
-},
-{
-path: "/",
-redirect: "home"
-},
-{
-path: "/about",
-name: "about",
-// route level code-splitting
-// this generates a separate chunk (About.[hash].js) for this route
-// which is lazy-loaded when the route is visited.
-component: () => import("../views/AboutView.vue"),
-},
-{
-path: "/devices",
-name: "devices",
-// route level code-splitting
-// this generates a separate chunk (About.[hash].js) for this route
-// which is lazy-loaded when the route is visited.
-component: () => import("../repair-it/pages/device-list.component.vue"),
-}
-],
-});
-export default router;
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home-view',
+            component: HomeView
+        },
+        {
+            path: '/about',
+            name: 'about',
+            // route level code-splitting
+            // this generates a separate chunk (About.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import('../views/AboutView.vue')
+        },
+        {
+            path: '/devices',
+            name: 'devices',
+            // route level code-splitting
+            // this generates a separate chunk (About.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: Devices
+        },
+        {
+            path: '/sign-in',
+            name: 'sign-in',
+            component: SigIn
+
+        }
+    ]
+})
+
+export default router
