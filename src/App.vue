f83d92360736e73d77dcd9f20fd0395f31f9b635
JCayllahuaG <u202019558@upc.edu.pe>
2022-09-28 20:31:10 -0500

    fix: files routes

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,42 @@
-<script>
+
+<template>
+  <div style="margin-bottom: 4rem">
+    <Toolbar></Toolbar>
+    <RouterView></RouterView>
+  </div>
+</template>
+
+<script >
+import Toolbar from "../src/repair-it/components/tool-bar.component.vue";
+
 export default {
-  data() {
-    return {
-      drawer: false,
-      items: [
-        { label: "Home", to: "/home" },
-        //        { label: "About", to: "/about" },
-        { label: "Devices", to: "/devices" },
-      ],
-    };
+  components: {
+    Toolbar,
   },
+  data() {
+
+  },
+  created() {
+  }
 };
 </script>
-<template>
-  <pv-toast />
-  <header>
-    <pv-toolbar class="bg-primary">
-      <template #start>
-        <pv-button
-          class="p-button-text text-white"
-          icon="pi pi-bars"
-          @click="drawer = !drawer"
-        ></pv-button>
-        <h3>Repair It</h3>
-      </template>
-      <template #end>
-        <div class="flex-column">
-          <router-link
-            v-for="item in items"
-            :to="item.to"
-            custom
-            v-slot="{ navigate, href }"
-            :key="item.label"
-          >
-            <pv-button
-              class="p-button-text text-white"
-              :href="href"
-              @click="navigate"
-              >{{ item.label }}</pv-button
-            >
-          </router-link>
-        </div>
-      </template>
-    </pv-toolbar>
-  </header>
-  <pv-sidebar v-model:visible="drawer"> </pv-sidebar>
-  <RouterView />
-</template>
+<style lang="scss">
+* {
+  margin: 0;
+}
+*::-webkit-scrollbar {
+  -webkit-appearance: none;
+}
+*::-webkit-scrollbar:vertical {
+  width: 7px;
+}
+*::-webkit-scrollbar-button:increment,
+*::-webkit-scrollbar-button {
+  display: none;
+}
+*::-webkit-scrollbar:horizontal {
+  height: 7px;
+}
+
+
+</style>
