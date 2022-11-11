1d53396507b3162c4431ecf5d9305cf21d8caef7
ludwin729 <ludwintac@gmail.com>
2022-11-10 22:27:49 -0500

    feat(myReports): add myReports script

--- src/components/customer/MyReports.vue
+++ src/components/customer/MyReports.vue
@@ -203,4 +203,74 @@
 
 
 </template>
+
+<script>
  
+ 
+import ProductService from "../../core/services/ProductService";
+
+
+
+
+
+
+export default {
+
+
+   data() {
+
+
+       return {
+
+
+           dataviewValue: null,
+
+
+           layout: 'grid'
+
+
+       }
+
+
+   },
+
+
+   productService: null,
+
+
+   created() {
+
+
+       this.productService = new ProductService();
+
+
+   },
+
+
+   mounted() {
+
+
+       this.productService.getProducts().then(data => this.dataviewValue = data);
+
+
+   }
+
+
+}
+
+
+</script>
+
+
+
+
+
+
+<style scoped lang="scss">
+
+
+@import '../../assets/demo/badges.scss';
+
+
+</style>
+
