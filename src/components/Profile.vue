478751c2a5c0652f7bb1b4b4653f84ee23e8882e
Futuri <u201924361@upc.edu.pe>
2022-11-10 19:23:06 -0500

    feat(profile): add profile template

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,5 +1,73 @@
 <template>
-    <div>
-        <h1>Profile</h1>
+    <div class="col-12">
+        <div class="card">
+            <div class="surface-section">
+                <div class="font-medium text-3xl text-900 mb-3">My Profile</div>
+                <div class="text-500 mb-5">You can see and edit your profile information here!</div>
+ 
+                <ul class="list-none p-0 m-0">
+                    <li class="flex align-items-center py-3 px-2 border-top-1 surface-border flex-wrap">
+                        <div class="col-12">
+                            <div class="text-500 w-6 md:w-2 font-medium">Profile Picture</div>
+                            <div class="flex justify-content-center">
+                                <pv-image src="images/galleria/galleria1.jpg" alt="Image" width="100" preview />
+                            </div>
+                        </div>
+                        <!--<div class="w-6 md:w-2 flex justify-content-end">
+                    <pv-button label="Edit" icon="pi pi-pencil" class="p-button-text"></pv-button>
+                </div>-->
+                    </li>
+                    <li class="flex align-items-center py-3 px-2 border-top-1 surface-border flex-wrap">
+                        <div class="text-500 w-6 md:w-2 font-medium">First Name</div>
+                        <div class="text-900 w-full md:w-8 md:flex-order-0 flex-order-1" v-for="user in users"> {{
+                                user.firstName
+                        }}</div>
+                        <!--<div class="w-6 md:w-2 flex justify-content-end">
+                    <pv-button label="Edit" icon="pi pi-pencil" class="p-button-text"></pv-button>
+                </div>-->
+                    </li>
+                    <li class="flex align-items-center py-3 px-2 border-top-1 surface-border flex-wrap">
+                        <div class="text-500 w-6 md:w-2 font-medium">Last Name</div>
+                        <div class="text-900 w-full md:w-8 md:flex-order-0 flex-order-1" v-for="user in users">{{
+                                user.lastName
+                        }}</div>
+                        <!--<div class="w-6 md:w-2 flex justify-content-end">
+                    <pv-button label="Edit" icon="pi pi-pencil" class="p-button-text"></pv-button>
+                </div>-->
+                    </li>
+                    <li class="flex align-items-center py-3 px-2 border-top-1 surface-border flex-wrap">
+                        <div class="text-500 w-6 md:w-2 font-medium">Email</div>
+                        <div class="text-900 w-full md:w-8 md:flex-order-0 flex-order-1" v-for="user in users">{{
+                                user.email
+                        }}</div>
+                        <!--<div class="w-6 md:w-2 flex justify-content-end">
+                    <pv-button label="Edit" icon="pi pi-pencil" class="p-button-text"></pv-button>
+                </div>-->
+                    </li>
+ 
+                    <li class="flex align-items-center py-3 px-2 border-top-1 surface-border flex-wrap">
+                        <div class="text-500 w-6 md:w-2 font-medium">Type</div>
+                        <div class="text-900 w-full md:w-8 md:flex-order-0 flex-order-1" v-for="user in users">
+                            <div v-if="user.isTechnician">Technician</div>
+                            <div v-else>Customer</div>
+                        </div>
+                    </li>
+                    <!--<li class="flex align-items-center py-3 px-2 border-top-1 surface-border flex-wrap">
+                <div class="text-500 w-6 md:w-2 font-medium">User Type</div>
+                <div class="text-900 w-full md:w-8 md:flex-order-0 flex-order-1" v-for="user in users">
+                    {{if(user.isTechnician) type="technician", else type = "customer" }}
+                    <pv-chip label=type class="mr-2"></pv-chip>
+ 
+                </div>
+                <div class="w-6 md:w-2 flex justify-content-end">
+                    <pv-button label="Edit" icon="pi pi-pencil" class="p-button-text"></pv-button>
+                </div>
+            </li>-->
+ 
+                </ul>
+            </div>
+        </div>
     </div>
 </template>
+
+
